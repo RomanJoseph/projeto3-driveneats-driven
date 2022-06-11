0d747069d5f8694d7eac28ccd4fa00d6dac8b63a
Shizuke136 <74119400+Shizuke136@users.noreply.github.com>
2022-06-11 19:23:45 -0300

    Adicionado checkbox ao layout do item selecionado

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function marcar(objeto,categoria){
-    const before = document.querySelector("."+ categoria + ".selecionado")
+    const before = document.querySelector("."+ categoria + ".selecionado");
+
     if(before != null){
         before.classList.remove("selecionado");
-    }
+      }
 
     objeto.classList.add("selecionado");
 
@@ -18,4 +19,19 @@ function fazerPedido(){
       document.querySelector(".button-nao-pode-comprar").style.display ="none";
       document.querySelector(".button-pode-comprar").style.display ="flex";
     }
+  }
+
+  function custo(preco){
+    return Number(preco.slice(0, -3)) + Number(preco.slice(-2)) / 100;
+  }
+
+  function fechar_pedido(){
+    const comida = document.querySelector(".comida.selecionado");
+    const bebida = document.querySelector(".bebida.selecionado");
+    const doce = document.querySelector(".doce.selecionado");
+    const total = (custo(comida.querySelector(".preco").textContent) + custo(bebida.querySelector(".preco").textContent) + custo(doce.querySelector(".preco").textContent)).toFixed(2);
+    
+    const pedido = `Olá, gostaria de fazer o pedido: \n -Prato: Salmão \n -Bebida: Coquinha Gelada \n Sobremesa: Sorvete \n Total: R$${total}`
+    window.open(`https://wa.me/5518988116427?text=${encodeURIComponent(pedido)}`)
+
   }
